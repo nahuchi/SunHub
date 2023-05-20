7bff539c4bd32fb555baa63620ae1e633197f0d9
nah? <133935086+nahuchi@users.noreply.github.com>
2023-05-20 20:43:57 +0700

    Update SunHub.lua

--- SunHub.lua
+++ SunHub.lua
@@ -6,6 +6,7 @@ local Window = OrionLib:MakeWindow({Name = "SubHub | Blox Fruit", HidePremium = 
 _G.AutoFarm = true
 _G.SelectWeapon = true
 _G.AutoHaki = true
+_G.CheckQuest = true
 --Funtions
 if game.PlaceId == 2753915549 then
     World1 = true
@@ -496,20 +497,444 @@ function CheckQuest()
         end
     end
 end
-
+--HakiBuso
 function AutoHaki()
+    while _G.AutoHaki == true do
     if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("HasBuso") then
         game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Buso")
     end
 end
+--Bypass TP Sea 1
+function WindMill()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(979.79895019531, 16.516613006592, 1429.0466308594)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function Marine()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2566.4296875, 6.8556680679321, 2045.2561035156)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function MiddleTown()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-690.33081054688, 15.09425163269, 1582.2380371094)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function Jungle()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1612.7957763672, 36.852081298828, 149.12843322754)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function PirateVillage()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1181.3093261719, 4.7514905929565, 3803.5456542969)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function Desert()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(944.15789794922, 20.919729232788, 4373.3002929688)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function SnowIsland()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1347.8067626953, 104.66806030273, -1319.7370605469)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function MarineFord()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-4914.8212890625, 50.963626861572, 4281.0278320313)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function Colosseum()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1427.6203613281, 7.2881078720093, -2792.7722167969)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function SkyIsland1()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-4869.1025390625, 733.46051025391, -2667.0180664063)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function SkyIsland2()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-4607.82275, 872.54248, -1667.55688)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function SkyIsland3()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-7894.6176757813, 5547.1416015625, -380.29119873047)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function Prison()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(4875.330078125, 5.6519818305969, 734.85021972656)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function MagmaVillage()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5247.7163085938, 12.883934020996, 8504.96875)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function UnderWaterIsland()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(61163.8515625, 11.6796875, 1819.7841796875)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function FountainCity()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5127.1284179688, 59.501365661621, 4105.4458007813)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function MobIsland()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2850.20068, 7.39224768, 5354.99268)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+--Bypass TP Sea 2
+function TheCafe()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-380.47927856445, 77.220390319824, 255.82550048828)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+function FristSpot()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-11.311455726624, 29.276733398438, 2771.5224609375)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end         
+function GreenZone()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2448.5300292969, 73.016105651855, -3210.6306152344)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end    
+function ZombieIsland()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5622.033203125, 492.19604492188, -781.78552246094)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function TwoSnowMoutain()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(753.14288330078, 408.23559570313, -5274.6147460938)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function PunkHazard()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-6127.654296875, 15.951762199402, -5040.2861328125)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function CursedShip()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(923.40197753906, 125.05712890625, 32885.875)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function IceCastle()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(6148.4116210938, 294.38687133789, -6741.1166992188)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function ForgottenIsland()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3032.7641601563, 317.89672851563, -10075.373046875)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end
+--Bypass TP Sea 3  
+function GreatTree()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2681.2736816406, 1682.8092041016, -7190.9853515625)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function CastleOnTheSea()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5074.45556640625, 314.5155334472656, -2991.054443359375)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function PortTown()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-290.7376708984375, 6.729952812194824, 5343.5537109375)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function HydraIsland()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5228.8842773438, 604.23400878906, 345.0400390625)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function FloatingTurtle()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-13274.528320313, 531.82073974609, -7579.22265625)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function Mansion()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(12471.169921875, 374.94024658203, -7551.677734375)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function HauntedCastle()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-9515.3720703125, 164.00624084473, 5786.0610351562)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function IceCreamIsland()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-902.56817626953, 79.93204498291, -10988.84765625)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function PeanutIsland()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2062.7475585938, 50.473892211914, -10232.568359375)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
+function CakeIsland()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1884.7747802734375, 19.327526092529297, -11666.8974609375)
+    wait(.01)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end  
 
 --Tabs
+local Tab = Window:MakeTab({
+	Name = "Teleport Bypass",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 local FarmTab = Window:MakeTab({
 	Name = "Farm",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
---Dropdown
+--Buttons
+if World1 then
+    Tab:AddButton({
+    Name = "WindMill",
+    Callback = function()
+        WindMill()
+    end    
+})
+Tab:AddButton({
+    Name = "Marine",
+    Callback = function()
+        Marine()
+    end    
+})
+Tab:AddButton({
+    Name = "Middle Town",
+    Callback = function()
+        MiddleTown()
+    end    
+})
+Tab:AddButton({
+    Name = "Jungle",
+    Callback = function()
+        Jungle()
+    end    
+})
+Tab:AddButton({
+    Name = "Pirate Village",
+    Callback = function()
+        PirateVillage()
+    end    
+})
+Tab:AddButton({
+    Name = "Desert",
+    Callback = function()
+        Desert()
+    end    
+})
+Tab:AddButton({
+    Name = "WindMill",
+    Callback = function()
+        WindMill()
+    end    
+})
+Tab:AddButton({
+    Name = "Snow Island",
+    Callback = function()
+        SnowIsland()
+    end    
+})
+Tab:AddButton({
+    Name = "MarineFord",
+    Callback = function()
+        MarineFord()
+    end    
+})
+Tab:AddButton({
+    Name = "Colosseum",
+    Callback = function()
+        Colosseum()
+    end    
+})
+Tab:AddButton({
+    Name = "Sky Island 1",
+    Callback = function()
+        SkyIsland1()
+    end    
+})
+
+    Tab:AddButton({
+    Name = "Sky Island 2",
+    Callback = function()
+        SkyIsland2()
+    end    
+})
+Tab:AddButton({
+    Name = "Sky Island 3",
+    Callback = function()
+        SkyIsland3()
+    end    
+})
+Tab:AddButton({
+    Name = "Prison",
+    Callback = function()
+        Prison()
+    end    
+})
+
+Tab:AddButton({
+    Name = "Magma Village",
+    Callback = function()
+        MagmaVillage()
+    end    
+})
+Tab:AddButton({
+    Name = "Under Water Island",
+    Callback = function()
+        UnderWaterIsland()
+    end    
+})
+Tab:AddButton({
+    Name = "Fountain City",
+    Callback = function()
+        FountainCity()
+    end    
+})
+
+Tab:AddButton({
+    Name = "Mob Island",
+    Callback = function()
+        MobIsland()
+    end    
+})
+
+if World2 then
+Tab:AddButton({
+    Name = "The Cafe",
+    Callback = function()
+        TheCafe()
+    end    
+})
+Tab:AddButton({
+    Name = "Frist Spot",
+    Callback = function()
+        FristSpot()
+    end    
+})
+Tab:AddButton({
+    Name = "Green Zone",
+    Callback = function()
+        GreenZone()
+    end    
+})
+
+    Tab:AddButton({
+    Name = "Zombie Island",
+    Callback = function()
+        ZombieIsland()
+    end    
+})
+Tab:AddButton({
+    Name = "Two Snow Mountain",
+    Callback = function()
+        TwoSnowMountain()
+    end    
+})
+Tab:AddButton({
+    Name = "Punk Hazard",
+    Callback = function()
+        PunkHazard()
+    end    
+})
+Tab:AddButton({
+    Name = "Cursed Ship",
+    Callback = function()
+        CursedShip()
+    end    
+})
+
+Tab:AddButton({
+    Name = "Ice Castle",
+    Callback = function()
+        IceCastle()
+    end    
+})
+Tab:AddButton({
+    Name = "Forgotten Island",
+    Callback = function()
+        ForgottenIsland()
+    end    
+})
+
+if World3 then
+Tab:AddButton({
+    Name = "Great Tree",
+    Callback = function()
+        GreatTree()
+    end    
+})
+Tab:AddButton({
+    Name = "Castle On The Sea",
+    Callback = function()
+        CastleOnTheSea()
+    end    
+})
+Tab:AddButton({
+    Name = "Port Town",
+    Callback = function()
+        PortTown()
+    end    
+})
+
+Tab:AddButton({
+    Name = "Hydra Island",
+    Callback = function()
+        HydraIsland()
+    end    
+})
+Tab:AddButton({
+    Name = "Floating Turtle",
+    Callback = function()
+        FloatingTurtle()
+    end    
+})
+Tab:AddButton({
+    Name = "Mansion",
+    Callback = function()
+        Mansion()
+    end    
+})
+Tab:AddButton({
+    Name = "Haunted Castle",
+    Callback = function()
+        HauntedCastle()
+    end    
+})
+Tab:AddButton({
+    Name = "Ice Cream Island",
+    Callback = function()
+        IceCreamIsland()
+    end    
+})
+Tab:AddButton({
+    Name = "Peanut Island",
+    Callback = function()
+        PeanutIsland()
+    end    
+})
+Tab:AddButton({
+    Name = "Cake Island",
+    Callback = function()
+        CakeIsland()
+    end    
+})
+  
+--Dropdowns
 WeaponList = {}
     
 for i,v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do  
@@ -526,7 +951,6 @@ FarmTab:AddDropdown({
           SelectWeapon()
 	end    
 })
-
 
 --Toggle
 FarmTab:AddToggle({
